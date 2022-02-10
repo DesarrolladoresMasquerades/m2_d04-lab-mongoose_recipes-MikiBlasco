da9dd67f84a571d3c0b87880f683bf119414f55b
Miki Blasco <blascom@7@gmail.com>
2022-02-10 17:07:39 +0100

    1

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
@@ -5,7 +6,8 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+//const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = `mongodb+srv://${process.env.USER}:${process.env.PWD}@cluster0.lpnm2.mongodb.net/Receipes_lab?retryWrites=true&w=majority`;
 
 // Connection to the database "recipe-app"
 mongoose
@@ -16,8 +18,33 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      title: "Asian stuff",
+      level: "Amateur Chef",
+      ingredients: [
+        "1/2 cup rice vinegar",
+        "5 tablespoons honey",
+        "1/3 cup soy sauce (such as Silver Swan®)",
+        "1/4 cup Asian (toasted) sesame oil",
+        "3 tablespoons Asian chili garlic sauce",
+        "3 tablespoons minced garlic",
+        "salt to taste",
+        "8 skinless, boneless chicken thighs"
+      ],
+      cuisine: "Asian",
+      dishType: "main_course",
+      image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+      duration: 40,
+      creator: "Chef LePapu"
+  })
+  .then((data)=>{
+    console.log(data.title)
+  })
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+
+
